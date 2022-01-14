b555353a0857f66b95f8ce8467d71d2b271ab2b2
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-01-14 10:48:21 +0530

    Sort a Stack

--- Recurssion Problem/src/Main.java
+++ Recurssion Problem/src/Main.java
@@ -1,59 +1,55 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
-    List<Integer> arr = new ArrayList<>() ;
-    arr.add(0) ;
-    arr.add(1) ;
-    arr.add(5) ;
-    arr.add(2) ;
+        Stack<Integer> myStack = new Stack<>() ;
+        myStack.push(0) ;
+        myStack.push(1) ;
+        myStack.push(5) ;
+        myStack.push(2) ;
 
-    int mid = (arr.size()  / 2 )+ 1 ;
-        System.out.println(mid);
-    for (int i : arr){
-        System.out.print(i + " ");
-    }
+        System.out.println("stack of top: " + myStack.peek());
+
+        System.out.println("Stack without Sort: ");
+        for(int i : myStack){
+            System.out.print(i + " ");
+        }
+        sortMyStack(myStack);
         System.out.println();
-    sort(arr);
-    for (int i : arr){
-        System.out.print(i + " ");
+        System.out.println("Stack with Sort: ");
+        for (int i : myStack){
+            System.out.print(i + " ");
+        }
     }
-
-    }
-
- public static void sort(List<Integer> arr){
-        // check array is empty or not
-     if(arr.size() == 0){
-         return ;
-     }
-     // base condition
-     if(arr.size() == 1){
-         return;
-     }
-     int temp = arr.get(arr.size() - 1);
-     arr.remove(arr.size() -1) ;
-     sort(arr);
-     insert(arr, temp) ;
-
- }
- public static void insert(List<Integer> arr, int temp){
-        if(arr.size() == 0 || arr.get(arr.size() - 1) <= temp){
-            arr.add(temp) ;
+    public static void sortMyStack(Stack<Integer> s){
+        if(s.size() == 1){
             return;
         }
-        int val = arr.get(arr.size() - 1);
-        arr.remove(arr.size() - 1) ;
-        insert(arr,temp);
-        arr.add(val) ;
+        int temp = s.peek() ;
+        s.pop() ;
+        sortMyStack(s);
+        insert(s, temp);
         return;
- }
-    
+    }
+    public static void insert(Stack<Integer> s, int temp) {
+        if (s.size() == 1 || s.size() <= temp) {
+            s.push(temp) ;
+            return;
+        }
+        int val = s.peek() ;
+        s.pop() ;
+        insert(s,temp);
+        s.push(val) ;
+        return;
+    }
 }
 
 /*
 OUTPUT:
+stack of top: 2
+Stack without Sort:
 0 1 5 2
+Stack with Sort:
 0 1 2 5
  */
