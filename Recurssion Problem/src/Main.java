233da0976af32c2ef88e6e4f267aa4f865e6a3db
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-01-14 18:09:02 +0530

    Reverse A Stack

--- Recurssion Problem/src/Main.java
+++ Recurssion Problem/src/Main.java
@@ -3,48 +3,57 @@ import java.util.Stack;
 public class Main {
     public static void main(String[] args) {
 
-        Stack<Integer> myStack = new Stack<>();
-        myStack.push(1);
-        myStack.push(2);
-        myStack.push(3);
-        myStack.push(4);
+        Stack<Integer> myStack = new Stack<>() ;
+        myStack.push(1) ;
+        myStack.push(2) ;
+        myStack.push(3) ;
+        myStack.push(4) ;
         myStack.push(5) ;
 
-        int mid =  (myStack.size() / 2 ) + 1 ;
-        System.out.println("Top of Stack: " + myStack.peek());
-        System.out.println("Middle Of Stack: " + mid);
-        System.out.println("Stack Before Deleting Middle Element:");
-        for(int i : myStack){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-        solve(myStack, mid);
-        System.out.println("Stack After Deleting Middle Element:");
+        System.out.println("Stack before reverse: ");
+
         for (int i : myStack){
             System.out.print(i + " ");
         }
+        System.out.println();
 
+        reverse(myStack);
 
+        System.out.println("Stack after reverse:");
+        for(int i : myStack){
+            System.out.print(i + " ");
+        }
 
     }
-    public static void solve(Stack<Integer> s, int k) {
-        if(k == 1){
-            s.pop() ;
+    public static void reverse(Stack<Integer> stack) {
+        if(stack.size() == 1){
             return;
         }
-        int temp = s.peek() ;
-        s.pop() ;
-        solve(s, k - 1);
-        s.push(temp) ;
+        int temp = stack.peek() ;
+        stack.pop() ;
+        reverse(stack);
+        insert(stack,temp);
+        return;
+    }
+    public static void insert(Stack<Integer> stack, int element) {
+        if(stack.size() == 0){
+            stack.push(element );
+            return;
+        }
+        int temp = stack.peek() ;
+        stack.pop() ;
+        insert(stack, element);
+        stack.push(temp) ;
         return;
     }
 }
 
 /*
-Top of Stack: 5
-Middle Of Stack: 3
-Stack Before Deleting Middle Element:
+OUTPUT:
+
+Stack before reverse:
 1 2 3 4 5
-Stack After Deleting Middle Element:
-1 2 4 5
+Stack after reverse:
+5 4 3 2 1
+
  */
