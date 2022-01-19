7dce602b7ccc7e4c0b88e7a116d98428a7abcdb3
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-01-19 09:57:13 +0530

    LeetCode TwoSum problem T(N)= n2

--- Recurssion Problem/src/Main.java
+++ Recurssion Problem/src/Main.java
@@ -1,59 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Stack;
 
 public class Main {
     public static void main(String[] args) {
 
-        Stack<Integer> myStack = new Stack<>() ;
-        myStack.push(1) ;
-        myStack.push(2) ;
-        myStack.push(3) ;
-        myStack.push(4) ;
-        myStack.push(5) ;
+        List<Integer> myArray = new ArrayList<>() ;
+        myArray.add(3) ;
+        myArray.add(3) ;
+        myArray.add(4 );
 
-        System.out.println("Stack before reverse: ");
-
-        for (int i : myStack){
-            System.out.print(i + " ");
-        }
-        System.out.println();
-
-        reverse(myStack);
-
-        System.out.println("Stack after reverse:");
-        for(int i : myStack){
-            System.out.print(i + " ");
-        }
+        twoSum(myArray,6);
 
     }
-    public static void reverse(Stack<Integer> stack) {
-        if(stack.size() == 1){
-            return;
+    public static void twoSum(List<Integer> a, int target){
+        for(int i = 0; i<a.size(); i++){
+           for(int j = i + 1; j<a.size();j++){
+               if (a.get(i) + a.get(j) == target){
+                   System.out.printf("%d %d%n", i, j);
+               }
+           }
         }
-        int temp = stack.peek() ;
-        stack.pop() ;
-        reverse(stack);
-        insert(stack,temp);
-        return;
-    }
-    public static void insert(Stack<Integer> stack, int element) {
-        if(stack.size() == 0){
-            stack.push(element );
-            return;
-        }
-        int temp = stack.peek() ;
-        stack.pop() ;
-        insert(stack, element);
-        stack.push(temp) ;
-        return;
     }
 }
-
-/*
-OUTPUT:
-
-Stack before reverse:
-1 2 3 4 5
-Stack after reverse:
-5 4 3 2 1
-
- */
