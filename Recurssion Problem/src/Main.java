1f2635afe12ff0504757786b5fc03ffa01b8ab7c
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-01-25 18:14:37 +0530

    BinarySearch T(n): log2n

--- Recurssion Problem/src/Main.java
+++ Recurssion Problem/src/Main.java
@@ -3,34 +3,47 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-       int[] a = new int[5] ;
-       a[0] = 2 ;
-       a[1] = 6 ;
-       a[2] = 5 ;
-       a[3] = 8 ;
-       a[4] = 11 ;
+        int[] a = new int[10];
+        a[0] = 1;
+        a[1] = 2;
+        a[2] = 3;
+        a[3] = 4;
+        a[4] = 5;
+        a[5] = 6;
+        a[6] = 7;
+        a[7] = 8;
+        a[8] = 9;
+        a[9] = 10;
 
-      int[] data =  new int[2] ;
-      data =twoSum(a,14) ;
-      for (int i: data){
-          System.out.print(i + " ");
-      }
+        int result = binarySearch(a, 10) ;
+        if (result == -1){
+            System.out.println("Element not found");
+        }else{
+            System.out.println("Element present at index: " + result );
 
-
-
-    }
-    public static int[] twoSum(int[] a, int target){
-        int[] result = new int[2] ;
-        Map<Integer, Integer> myMap = new HashMap<Integer, Integer>() ;
-
-        for(int i = 0 ; i<a.length; i++){
-            if(myMap.containsKey(target - a[i])) {
-                result[1] = i ;
-                result[0] = myMap.get(target - a[i]) ;
-                return result ;
-            }
-            myMap.put(a[i] ,i ) ;
         }
-        return result ;
+    }
+    public static int binarySearch(int[] a, int element){
+        int start = 0 ;
+        int end = a.length - 1 ;
+        int mid ;
+        while(start <= end){
+            mid = start + (end - start ) / 2 ;
+            if (element == a[mid]){
+                return mid ;
+            }
+           else if (element < a[mid]){
+                end = mid - 1 ;
+            }else {
+                start = mid + 1 ;
+            }
+        }
+        return  -1 ;
     }
 }
+/*
+OUTPUT:
+
+Element present at index: 9
+
+ */
