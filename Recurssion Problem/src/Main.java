71af18a6160ce211b7ad179a3c7f380341341947
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-01-26 08:49:07 +0530

    First Occurrence of an Element

--- Recurssion Problem/src/Main.java
+++ Recurssion Problem/src/Main.java
@@ -3,43 +3,47 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        int[] a = new int[10];
-        a[0] = 20;
-        a[1] = 19;
-        a[2] = 17;
-        a[3] = 15;
-        a[4] = 14;
-        a[5] = 12;
-        a[6] = 10;
-        a[7] = 9;
-        a[8] = 8;
-        a[9] = 4;
+        int[] a = new int[9];
+       a[0] = 2 ;
+       a[1] = 4;
+       a[2] = 10 ;
+       a[3] = 10 ;
+       a[4] = 10 ;
+       a[5] = 10 ;
+       a[6] = 10 ;
+       a[7] = 18 ;
+       a[8] = 20 ;
 
-        int result = binarySearchOnReversedSortedArray(a,14) ;
-        System.out.println(result);
+       int element = 10 ;
+       int firstOccurance = firstOccuranceUsingBinarySearch(a, element) ;
+        System.out.println("First occurance of element-> " +  element  + " present at index: " +firstOccurance);
+
 
     }
-    public static int binarySearchOnReversedSortedArray(int[] a, int element){
-        int start = 0 ;
+    public static int firstOccuranceUsingBinarySearch(int[] a, int element){
+        int start =  0;
         int end = a.length - 1 ;
         int mid ;
+        int result = 0;
+
         while(start <= end){
             mid = start + (end - start) / 2 ;
             if (element == a[mid]){
-                return mid ;
-            }
-            if (element < a[mid]){
-                start = mid + 1 ;
-            }else{
+                result = mid ;
                 end = mid - 1 ;
             }
+            else if (element < a[mid]){
+                end = mid - 1 ;
+            }else {
+                start = mid + 1 ;
+            }
         }
-        return  -1 ;
+        return result ;
     }
 }
 /*
 OUTPUT:
 
-Element present at index: 4
+First occurance of element-> 10 present at index: 2
 
  */
