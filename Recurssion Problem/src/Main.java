71824403cda098d26e94c56072e059c391d30d48
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-01-26 17:04:59 +0530

    Searching in Nearly Sorted Array

--- Recurssion Problem/src/Main.java
+++ Recurssion Problem/src/Main.java
@@ -3,72 +3,46 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        int[] a = new int[9];
-        a[0] = 11;
-        a[1] = 12;
-        a[2] = 15;
-        a[3] = 18;
-        a[4] = 2;
-        a[5] = 5;
-        a[6] = 6;
-        a[7] = 8;
+        int[] a = new int[5];
+      a[0] = 5 ;
+      a[1] = 10 ;
+      a[2] = 30 ;
+      a[3] = 20 ;
+      a[4] = 40 ;
 
-       int result = findNoOfTimesArrayIsRotated(a);
-       System.out.println("Number of times a sorted array is rotated: " + result);
-       findElementInSortedRotatedArray(a,18);
+      int result = findElementInNearlySortedArray(a,40 );
+        System.out.println("Searching in nearly sorted Array: "+result);
 
     }
-    public static void findElementInSortedRotatedArray(int[] a, int element){
-        int index = findNoOfTimesArrayIsRotated(a) ;
-        int firstHalf = binarySearch(a,0,index - 1, element) ;
-        int secondHalf = binarySearch(a,index + 1,a.length - 1 , element) ;
-        System.out.println("first half " + firstHalf);
-        System.out.println("second half " + secondHalf);
-    }
-
-    public static int binarySearch(int[] a, int start , int end, int element){
+    public static int findElementInNearlySortedArray(int[] a, int target){
+        // modified binary search
+        int start = 0 ;
+        int end = a.length - 1 ;
         int mid ;
-        while(start <= end){
+        while (start <= end){
+            mid = start + (end - start) / 2 ;
 
-            mid = start + (end - start) / 2;
-
-            if (element == a[mid]){
+            if(target == a[mid]){
                 return mid ;
-            }else if(element < a[mid]){
-                end = mid - 1 ;
-            }else {
-                start = mid + 1 ;
+            }
+            if(mid - 1 >= start && a[mid - 1] == target){
+                return mid - 1;
+            }
+            if (mid + 1 <= end && a[mid + 1]== target){
+                return mid + 1 ;
+            }
+            if(target < a[mid]){
+                end = mid - 2;
+            }else{
+                start = mid + 2 ;
             }
         }
         return -1 ;
-    }
-    public static int findNoOfTimesArrayIsRotated(int[] a){
-        int start = 0 ;
-        int end = a.length - 1 ;
-        int mid, next, prev;
-        int N = a.length ;
-        while (start <= end){
-            mid = start + (end - start) / 2 ;
-            next = (mid + 1) % N ;
-            prev = (mid + N -1) % N ;
-
-            if (a[mid] <= a[next] && a[mid] <= a[prev]) {
-                return mid ;
-            }
-            if (a[start] < a[mid]){
-                start = mid + 1 ;
-            }else if(a[mid] < a[end]){
-                end = mid - 1 ;
-            }
-        }
-        return  -1 ;
     }
 }
 /*
 OUTPUT:
 
-Number of times a sorted array is rotated: 4
-first half 2
-second half -1
+Searching in nearly sorted Array: 4
 
  */
