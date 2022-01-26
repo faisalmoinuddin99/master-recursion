113bb557ba2afbebc2ba40a535fe01cdd6073107
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-01-26 09:08:04 +0530

    Count of an Element in a Sorted Array

--- Recurssion Problem/src/Main.java
+++ Recurssion Problem/src/Main.java
@@ -10,15 +10,41 @@ public class Main {
        a[3] = 10 ;
        a[4] = 10 ;
        a[5] = 10 ;
-       a[6] = 11 ;
+       a[6] = 10 ;
        a[7] = 18 ;
        a[8] = 20 ;
 
        int element = 10 ;
+       int firstOccurrence = firstOccurrenceUsingBinarySearch(a, element) ;
        int lastOccurrence = lastOccurrenceUsingBinarySearch(a, element) ;
-        System.out.println("Last occurrence of element-> " +  element  + " present at index: " + lastOccurrence);
+
+        System.out.println("First Occurrence: "+ firstOccurrence);
+        System.out.println("Last Occurrence: "+lastOccurrence);
+
+        int count = lastOccurrence - firstOccurrence + 1 ;
+
+        System.out.println("Total Count of "+ element + " is: "+count) ;
 
 
+    }
+
+    public static int firstOccurrenceUsingBinarySearch(int[] a, int element){
+        int start = 0 ;
+        int end = a.length - 1 ;
+        int mid ;
+        int result = 0 ;
+        while (start <= end){
+            mid = start + (end - start) / 2 ;
+            if (element == a[mid]){
+                result = mid ;
+                end = mid - 1 ;
+            }else  if (element < a[mid]){
+                end = mid - 1 ;
+            }else{
+                start = mid + 1 ;
+            }
+        }
+        return result ;
     }
     public static int lastOccurrenceUsingBinarySearch(int[] a, int element){
         int start =  0;
@@ -40,10 +66,14 @@ public class Main {
         }
         return result ;
     }
+
+
 }
 /*
 OUTPUT:
 
-Last occurrence of element-> 10 present at index: 5
+First Occurrence: 2
+Last Occurrence: 6
+Total Count of 10 is: 5
 
  */
