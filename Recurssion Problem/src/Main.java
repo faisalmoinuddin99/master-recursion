93c2ea690f5ca18d8263af86c225e9be53c66348
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-01-26 11:13:11 +0530

    Find an Element in a sorted Rotated Array

--- Recurssion Problem/src/Main.java
+++ Recurssion Problem/src/Main.java
@@ -13,38 +13,62 @@ public class Main {
         a[6] = 6;
         a[7] = 8;
 
-        int result = findNoOfTimesArrayIsRotated(a) ;
-        System.out.println("Number of times a sorted array is rotated: "+result);
+       int result = findNoOfTimesArrayIsRotated(a);
+       System.out.println("Number of times a sorted array is rotated: " + result);
+       findElementInSortedRotatedArray(a,18);
 
     }
+    public static void findElementInSortedRotatedArray(int[] a, int element){
+        int index = findNoOfTimesArrayIsRotated(a) ;
+        int firstHalf = binarySearch(a,0,index - 1, element) ;
+        int secondHalf = binarySearch(a,index + 1,a.length - 1 , element) ;
+        System.out.println("first half " + firstHalf);
+        System.out.println("second half " + secondHalf);
+    }
 
-    public static int findNoOfTimesArrayIsRotated(int[] a) {
-        int start = 0;
-        int end = a.length - 1;
-        int mid;
-        int prev, next;
-        int N = a.length;
+    public static int binarySearch(int[] a, int start , int end, int element){
+        int mid ;
+        while(start <= end){
 
-        while (start <= end) {
             mid = start + (end - start) / 2;
-            prev = (mid + N - 1) % N;
-            next = (mid + 1) % N;
 
-            if (a[mid] <= a[next] && a[mid] <= a[prev]) {
-                return mid;
-            }
-            if (a[start] < a[mid]) {
-                start = mid + 1;
-            } else if (a[mid] < a[end]) {
-                end = end - 1;
+            if (element == a[mid]){
+                return mid ;
+            }else if(element < a[mid]){
+                end = mid - 1 ;
+            }else {
+                start = mid + 1 ;
             }
         }
-        return -1;
+        return -1 ;
+    }
+    public static int findNoOfTimesArrayIsRotated(int[] a){
+        int start = 0 ;
+        int end = a.length - 1 ;
+        int mid, next, prev;
+        int N = a.length ;
+        while (start <= end){
+            mid = start + (end - start) / 2 ;
+            next = (mid + 1) % N ;
+            prev = (mid + N -1) % N ;
+
+            if (a[mid] <= a[next] && a[mid] <= a[prev]) {
+                return mid ;
+            }
+            if (a[start] < a[mid]){
+                start = mid + 1 ;
+            }else if(a[mid] < a[end]){
+                end = mid - 1 ;
+            }
+        }
+        return  -1 ;
     }
 }
 /*
 OUTPUT:
 
 Number of times a sorted array is rotated: 4
+first half 2
+second half -1
 
  */
