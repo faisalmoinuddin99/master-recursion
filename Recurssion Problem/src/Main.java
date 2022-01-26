2e8bdb16281cf024ead8952bed3df50b8ac81246
faisalmoinuddin99 <faisal25march99@gmail.com>
2022-01-26 06:56:29 +0530

    Binary Search on Reveresed Sorted Array

--- Recurssion Problem/src/Main.java
+++ Recurssion Problem/src/Main.java
@@ -4,38 +4,34 @@ public class Main {
     public static void main(String[] args) {
 
         int[] a = new int[10];
-        a[0] = 1;
-        a[1] = 2;
-        a[2] = 3;
-        a[3] = 4;
-        a[4] = 5;
-        a[5] = 6;
-        a[6] = 7;
-        a[7] = 8;
-        a[8] = 9;
-        a[9] = 10;
+        a[0] = 20;
+        a[1] = 19;
+        a[2] = 17;
+        a[3] = 15;
+        a[4] = 14;
+        a[5] = 12;
+        a[6] = 10;
+        a[7] = 9;
+        a[8] = 8;
+        a[9] = 4;
 
-        int result = binarySearch(a, 10) ;
-        if (result == -1){
-            System.out.println("Element not found");
-        }else{
-            System.out.println("Element present at index: " + result );
+        int result = binarySearchOnReversedSortedArray(a,14) ;
+        System.out.println(result);
 
-        }
     }
-    public static int binarySearch(int[] a, int element){
+    public static int binarySearchOnReversedSortedArray(int[] a, int element){
         int start = 0 ;
         int end = a.length - 1 ;
         int mid ;
         while(start <= end){
-            mid = start + (end - start ) / 2 ;
+            mid = start + (end - start) / 2 ;
             if (element == a[mid]){
                 return mid ;
             }
-           else if (element < a[mid]){
-                end = mid - 1 ;
-            }else {
+            if (element < a[mid]){
                 start = mid + 1 ;
+            }else{
+                end = mid - 1 ;
             }
         }
         return  -1 ;
@@ -44,6 +40,6 @@ public class Main {
 /*
 OUTPUT:
 
-Element present at index: 9
+Element present at index: 4
 
  */
